40c26b1e7e884c7515798badbdd4690708b23e16
Jordan Rhinerson <jordan.rhinerson@gmail.com>
2025-08-06 15:28:16 -0400

    Install FontAwesome and add Social Icons

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgOptimizedImage } from '@angular/common';
 import { ViewportScroller } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgOptimizedImage],
+  imports: [RouterOutlet, NgOptimizedImage, FontAwesomeModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'rhinerson_portfolio';
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
+  faFile = faFile;
   constructor(private viewportScroller: ViewportScroller) {}
 
   scrollToElement(elementId: string): void {
