8b032e0b8f70a472e1081f471297b50e2bae0f2b
Jordan Rhinerson <jordan.rhinerson@gmail.com>
2025-08-08 13:08:07 -0400

    Add footer and hero sections

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+    faChevronRight = faChevronRight;
+    constructor(private viewportScroller: ViewportScroller) {}
 
+    scrollToElement(elementId: string): void {
+    this.viewportScroller.scrollToAnchor(elementId);}
 }
